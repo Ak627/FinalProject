864d102e62cb5ed248b34a1708330b3d344ceba5
Ak627 <91678317+Ak627@users.noreply.github.com>
2021-11-15 09:46:21 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,16 +5,18 @@ using namespace std;
 
 void Monsters();
 void Shop();
+
 int health = 100;
-int coins = 0;
+int coins = 10;
+int turns = 0;
 string Inventory[10] = { "wool shirt","wool pants","animal skin shoes","sharpened stick" };
 
 int main() {
 	int room = 1;
 	string input;
 	system("title The Lost Adventurer");
-	cout << "You wake up and find yourself in a mysterious forest. Can you figure out the way out? Will you find the treasure that is rumoured to be in this forest? Good luck adventurer have fun." <<  endl;
-	cout << "You see an opening through the trees." <<  endl;
+	cout << "You wake up and find yourself in a mysterious forest. Can you figure out the way out? Will you find the treasure that is rumoured to be in this forest? Good luck adventurer have fun." << endl;
+	cout << "You see an opening through the trees." << endl;
 	cout << "(To quit at any point in the game enter q)\n";
 	do {
 		cout << "Inventory: ";
@@ -23,10 +25,13 @@ int main() {
 		cout << endl;
 		cout << "Health: " << health << endl;
 		cout << "Coin Count: " << coins << endl;
+		cout << "Turns taken: " << turns << endl;
+		cout << "You have " << 50 - turns << " turns left." << endl;
+		turns++;
 		switch (room) {
 		case 1:
 			system("COLOR 2B");
-			cout << "You enter the opening you see. You can head North." <<  endl;
+			cout << "You enter the opening you see. You can head North." << endl;
 			cin >> input;
 			if (input == "North")
 				room = 2;
@@ -35,7 +40,7 @@ int main() {
 				return 0;
 			}
 			else
-				 cout << "trees block the path this way" << endl;
+				cout << "trees block the path this way" << endl;
 			break;
 		case 2:
 			system("COLOR 2B");
@@ -108,18 +113,18 @@ int main() {
 				return 0;
 			}
 			else
-				cout << "You stand there awkwardly with the villagers" <<  endl;
+				cout << "You stand there awkwardly with the villagers" << endl;
 			break;
 		case 6:
 			system("COLOR 2E");
-			cout << "You walk towards the small light." <<  endl;
-			cout << "The light is a very lovely looking fountain filled with mystical creatures." <<  endl;
-			cout << "The creatures seem very interested in you, would you like to Stay or keep heading East?" <<  endl;
+			cout << "You walk towards the small light." << endl;
+			cout << "The light is a very lovely looking fountain filled with mystical creatures." << endl;
+			cout << "The creatures seem very interested in you, would you like to Stay or keep heading East?" << endl;
 			cin >> input;
 			if (input == "Stay") {
-					system("COLOR 48");
-					MessageBox(nullptr, TEXT("YOU DIED."), TEXT("The creatures were hungry."), MB_OK);
-					return 0;
+				system("COLOR 48");
+				MessageBox(nullptr, TEXT("YOU DIED."), TEXT("The creatures were hungry."), MB_OK);
+				return 0;
 			}
 			else if (input == "East")
 				room = 11;
@@ -130,7 +135,7 @@ int main() {
 				return 0;
 			}
 			else
-				cout << "Please make a decision." <<  endl;
+				cout << "Please make a decision." << endl;
 			break;
 		case 7:
 			system("COLOR 0F");
@@ -143,7 +148,7 @@ int main() {
 			else if (input == "South")
 				room = 14;
 			else if (input == "West")
-				room = 16;
+				room = 15;
 			else if (input == "East")
 				room = 3;
 			else if (input == "q") {
@@ -250,6 +255,7 @@ int main() {
 			break;
 		case 14:
 			system("COLOR 0F");
+			Monsters();
 			cout << "You go South, there is an opening to the West and a path back North.\n";
 			cout << "A strange light seeps through from the West.\n";
 			cin >> input;
@@ -265,18 +271,84 @@ int main() {
 				cout << "It's cold and dark, nothing happens.\n";
 			break;
 		case 15:
+			system("COLOR 0F");
+			Monsters();
+			cout << "You head West, there is an path North, East, and West." << endl;
+			cin >> input;
+			if (input == "West")
+				room = 16;
+			else if (input == "North")
+				room = 18;
+			else if (input == "East")
+				room = 7;
+			else
+				cout << "It's cold and dark, nothing happens.\n";
 			break;
 		case 16:
+			system("COLOR 0F");
+			Monsters();
+			cout << "You head West, there is an path North, and East." << endl;
+			cin >> input;
+			if (input == "North")
+				room = 19;
+			else if (input == "East")
+				room = 15;
+			else
+				cout << "It's cold and dark, nothing happens.\n";
 			break;
+			
 		case 17:
+			system("COLOR 0F");
+			Monsters();
+			cout << "You head North, There is a path South and West." << endl;
+			cin >> input;
+			if (input == "South")
+				room = 7;
+			else if (input == "West")
+				room = 22;
+			else
+				cout << "It's cold and darl. Nothing happens." << endl;
 			break;
 		case 18:
+			system("COLOR 0F");
+			Monsters();
+			cout << "You head down another path in the cave, there is an path South, East, and West." << endl;
+			cin >> input;
+			if (input == "West")
+				room = 19;
+			else if (input == "South")
+				room = 15;
+			else if (input == "East")
+				room = 17;
+			else
+				cout << "It's cold and dark, nothing happens.\n";
 			break;
 		case 19:
+			system("COLOR 0F");
+			Monsters();
+			cout << "You head to a corner of the cave, there are paths South and East." << endl;
+			cin >> input;
+			if (input == "South")
+				room = 16;
+			else if (input == "East")
+				room = 18;
+			else
+				cout << "It's cold and dark, nothing happens.\n";
 			break;
 		case 20:
 			break;
 		case 21:
+			cout << "You go towards the light, you are on top of the cave." << endl;
+			cout << "You see the source of the light coming from a strange doorway. Would you like to enter? Y/N" << endl;
+			cin >> input;
+			if (input == "Y") {
+				int portal = rand() % 24 + 1;
+				room = portal;
+			}
+			else if (input == "N")
+				room = 14;
+			else
+				cout << "You stand there mesmerized by the portal." << endl;
 			break;
 		case 22:
 			break;
@@ -285,16 +357,36 @@ int main() {
 		case 24:
 			break;
 		}
-	} while (input != "q");
+	} while (input != "q" && turns < 50 && health > 0);
+	if (turns == 50) {
+		system("COLOR 48");
+		MessageBox(nullptr, TEXT("YOU DIED."), TEXT("You starved to death in the harsh wilderness."), MB_OK);
+		return 0;
+	}
+	else if (health == 0) {
+		system("COLOR 48");
+		MessageBox(nullptr, TEXT("YOU DIED."), TEXT("You were killed!"), MB_OK);
+		return 0;
+	}
 }
 void Monsters() {
 	int num = rand() % 100 + 1;
-	if (num <= 20)
-		cout << "A skeleton appears!" << endl << endl;
-	else if (num <= 50)
-		cout << "a zombie appears!" << endl << endl;
-	else if (num <= 90)
-		cout << "A group of goblins raid you!" << endl << endl;
+	int healthsub = rand() % 20 +1;
+	if (num <= 45) {
+		cout << "A skeleton appears and smacks you!" << endl;
+		health -= 10;
+		cout << "You get hit for 10 hp." << endl << endl;
+	}
+	else if (num <= 35) {
+		cout << "a zombie appears and bites you at an attempt to eat your brains!" << endl;
+		health -= healthsub;
+		cout << "You get hit for " << healthsub << " hp." << endl << endl;
+	}
+	else if (num <= 20) {
+		cout << "A group of goblins raid you and steal all your coins!" << endl;
+		coins -= coins;
+		cout << "You have 0 coins now." << endl << endl;
+	}
 	else
 		cout << "Nothing happens, it's quiet..." << endl << endl;
 }
